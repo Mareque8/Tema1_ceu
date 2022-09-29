011152188f4af083f88c55f0732b51adda9084b9
Mareque8 <113630042+Mareque8@users.noreply.github.com>
2022-09-29 10:51:01 +0200

    Update Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,8 @@
+import random
+numero_magico = 12345679
+numero_usuario = random.randint(1,9)
+print("El numero del usuario es: ", numero_usuario)
+multiplicacion = numero_usuario * 9
+print("El numero usuario por 9 es: ", multiplicacion)
+valor = numero_magico * numero_usuario
+print("El resultado es: ", valor)
