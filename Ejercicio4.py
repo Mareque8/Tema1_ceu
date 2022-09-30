f35573ee77b51699556668e8c1816da08c539b96
Mareque8 <mareque8marcos@gmail.com>
2022-09-30 19:05:37 +0200

    dfsff

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,4 +1,11 @@
 def ordenar(c):
-  return c["prioridad]
+  return c["prioridad"]
 cola = [
-  {"tarea4": "estudiar", "prioridad":"1" }
+  {"tarea4": "estudiar", "prioridad":"4" },
+  {"tarea3": "comer", "prioridad":"3"},
+  {"tarea2": "dormir", "prioridad":"2"},
+  {"tarea1": "beber", "prioridad":"1"},
+]
+
+cola.sort(key=ordenar)
+print(cola[0]["tarea1"],cola[1]["tarea2"],cola[2]["tarea3"],cola[3]["tarea4"])  
